aa173a004648ca0227e546000485d55f578667b9
ashish <ashishguptaid@gmail.com>
2020-06-17 23:01:39 +0530

    initialized repo | commited all the files

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_practice
+{
+    class Program
+    {
+        public struct myStruct {
+            public int x;
+        }
+        static void Main(string[] args)
+        {
+            myStruct p1;
+            p1.x = 2;
+            Console.WriteLine("p1.x" + p1.x);
+
+            myStruct p2 = p1;
+            p2.x = 3;
+            Console.WriteLine( "p2.x  = " + p2.x);
+            Console.ReadLine();
+        }
+    }
+}
